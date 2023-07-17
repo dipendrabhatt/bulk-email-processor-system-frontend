b5d819db782578bda204d4b63efe970aa0ad7943
HPDipendra <45689181+HPDipendra@users.noreply.github.com>
2023-07-17 15:11:29 +0545

    changes : added route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import { Homepage } from "./ui/pages/Homepage"
+import { UserLoginPage } from "./ui/pages/UserLoginPage"
+import { UserSignUpPage } from "./ui/pages/UserSignUpPage"
+import { VerifyEmailPage } from "./ui/pages/VerifyEmailPage"
 
 
 function App() {
   return (
     <>
       <div>
-        Hello World !!
+
+        <Routes>
+          <Route path="/" element={<UserLoginPage />} />
+          <Route path="/sign-up" element={<UserSignUpPage />} />
+          <Route path="/verify-email/" element={<VerifyEmailPage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+        <ToastContainer position="bottom-left" className="toast-container" />
       </div>
     </>
   )
